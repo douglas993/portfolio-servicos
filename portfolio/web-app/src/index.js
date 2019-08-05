dde016360ed574ffa1370e77d124b4ab7413e021
douglas993 <douglas.studies@gmail.com>
2019-08-05 16:50:52 -0300

    Subindo o inicio do CRUD WEB-APP

---
+++ portfolio/web-app/src/index.js
@@ -0,0 +1,31 @@
+import 'bootstrap';
+import scss from './scss/base.scss';
+import base from './css/base.css';
+
+
+//const teste = require('./promise')
+
+const es = "ES2019";
+const td = "Você clicou no botão"
+console.log(es);
+var x, y, z;
+x = 0, y = 0, z = 0;
+var rgb = 'rgb(' + x + ',' + y + ',' + z + ')'
+
+// // window.addEventListener("load",()=>{
+// //     alertButton();
+// // });
+// function alertButton(){
+//     alert("Clica no botão ai");
+// }
+function testeButton() {
+    console.log(td);
+    var i = 0;
+    setInterval(() => {
+            document.querySelector("body > h1").style.color = rgb;
+            x += 2 , y -= 1, z += 6;
+            rgb = 'rgb(' + x + ',' + y + ',' + z + ')'
+    }, 500);
+}
+
+document.querySelector("body > button").addEventListener("click", testeButton);
